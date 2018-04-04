cba3798cebdc7b8b1e82a9e895b049ceba58fdee
ETurns <ETurns@ETURNS>
2018-04-04 07:56:39 -0400

    Add project files.

---
+++ NMZ Timer/NMZTimer.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class NMZTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NMZTimer));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(115, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(109, 32);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(158, 73);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "5:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 151);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnStop);
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Location = new System.Drawing.Point(0, 113);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(369, 151);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(205, 53);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(134, 59);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(35, 53);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(134, 59);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblWarning.Location = new System.Drawing.Point(109, 120);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(167, 20);
+            this.lblWarning.TabIndex = 5;
+            this.lblWarning.Text = "DRINK OVERLOAD";
+            this.lblWarning.Visible = false;
+            // 
+            // NMZTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(369, 261);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NMZTimer";
+            this.Text = "NMZ Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ NMZ Timer/NMZTimer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    //Plays a sound when 5 minutes has passed, plays again every 30 seconds if not reset
+    public partial class NMZTimer : Form
+    {
+        //Var declarations
+        SoundPlayer alert;
+        Timer time;
+        float timeLeft;
+        float maxTime;
+        string defaultTime;
+
+        public NMZTimer()
+        {
+            InitializeComponent();
+        }
+
+        //Initializes variables and centers UI objects
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            time = new Timer();
+            alert = new SoundPlayer(Properties.Resources.alert);
+            time.Tick += new EventHandler(TimerTick);
+
+            panel2.Visible = false;
+            maxTime = 300;
+            time.Interval = 1000;
+            defaultTime = "5:00";
+
+            lblWarning.Location = new Point((this.Width / 2) - (lblWarning.Size.Width / 2), lblWarning.Location.Y);
+            button1.Location = new Point((this.Width / 2) - (button1.Size.Width / 2), button1.Location.Y);
+            btnReset.Location = new Point((this.Width / 2) - (btnReset.Size.Width / 2) - 85, btnReset.Location.Y);
+            btnStop.Location = new Point((this.Width / 2) - (btnStop.Size.Width / 2) + 85, btnStop.Location.Y);
+
+            CenterTime();
+        }
+
+        //Called once per 1000ms, updates timer and performs functions when time has passed 5 minutes
+        void TimerTick(object sender, EventArgs e)
+        {
+            timeLeft -= 1;
+
+            TimeSpan t = TimeSpan.FromMinutes(Convert.ToDouble(timeLeft));
+            string timeFormatted = t.ToString(@"hh\:mm");
+
+            if (timeLeft == 0 || timeLeft == -2 || timeLeft == -4 || timeLeft == -30 || timeLeft == -32 || timeLeft == -34)
+            {
+                alert.Play();
+            }
+
+            if (timeLeft <= 0)
+            {
+                lblWarning.Visible = !lblWarning.Visible;
+                timeFormatted = "- " + timeFormatted;
+            }
+
+            lblTime.Text = timeFormatted;
+            CenterTime();
+        }
+
+        //Start button, swaps visible UI and resets time
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+
+            timeLeft = maxTime;
+            time.Enabled = true;
+        }
+
+        //Resets timer
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timeLeft = maxTime;
+            lblTime.Text = defaultTime;
+            lblWarning.Visible = false;
+            CenterTime();
+        }
+
+        //Stops timer and returns to main menu
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timeLeft = maxTime;
+            lblTime.Text = defaultTime;
+
+            time.Enabled = false;
+            panel1.Visible = true;
+            panel2.Visible = false;
+            lblWarning.Visible = false;
+
+            CenterTime();
+        }
+
+        //Calculates center position, as the size of the label changes as the clock ticks down
+        void CenterTime()
+        {
+            lblTime.Location = new Point((this.Width / 2) - (lblTime.Size.Width / 2), lblTime.Location.Y);
+        }
+    }
+}
